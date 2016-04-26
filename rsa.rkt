c327c304e8a647384050bc131873f0389892de62
Michael Bowe <michael_bowe@student.uml.edu>
2016-04-25 22:32:15 -0400

    rename db procedure to make-db
    db is now a default instantiation of make-db
    usage:
    
    insert - (db 'insert '("data-item1" ... "data-itemN"))
    retrieve - (db 'retrieve "data-item1" 'passwd)
    show encrypted data - (db 'showdata)
    get whole db - (db 'get-all 'passwd)
    
    Notes:
    * inserts are for a list.
    * retrieve searches for a string as the car of the list. 
       - In essence, "data-item1" acts as a key for searches and any other data is auxillary
    * get-all outputs the data as list of lists
    * showdata outputs the data as it is stored.

--- rsa.rkt
+++ rsa.rkt
@@ -51,7 +51,7 @@
   (iter n ""))
 
 ;; database
-(define db
+(define make-db
   (lambda (p q pass)
     (define data '())
     (define internal-encryption (make-rsa p q pass))
@@ -77,10 +77,22 @@
                                 (list (car x))))
                              data)))
             "ERROR: wrong password")))
+    (define get-all
+      (lambda (passwd)
+        (if (equal? pass passwd)
+            (map (lambda (x) (map decode x))
+                 (map (lambda (x) (internal-encryption 'decrypt x pass))
+                      data))
+            "ERROR: wrong password")))
     (define (dispatch m . args)
       (cond ((eq? m 'insert) (insert args))
             ((eq? m 'retrieve) (retrieve (car args) (cadr args)))
+            ((eq? m 'showdata) data)
+            ((eq? m 'get-all) (get-all (car args)))
             (else "ERROR: invalid operation")))
     (if (and (prime? p) (prime? q))
         dispatch
         "ERROR: p and q must be prime")))
+
+;; instantiate a database
+(define db (make-db (+ (expt 10 999) 7) (+ (expt 10 999) 663) 'foo))
